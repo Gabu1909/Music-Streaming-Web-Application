4faa54191c410ad61a288f9939bc10f657d82e31
B2206005 <tuphuongdamdoi@gmail.com>
2025-06-21 18:47:44 +0700

    Update playlist.controller.js

--- backend-api/src/controllers/playlist.controller.js
+++ backend-api/src/controllers/playlist.controller.js
@@ -1,11 +1,19 @@
 const playlistService = require("../services/playlist.service");
-const { updatePlaylistSchema } = require("../schemas/playlist.schema");
+const {
+  createPlaylistSchema,
+  updatePlaylistSchema,
+} = require("../schemas/playlist.schema");
 const JSend = require("../jsend");
+
 async function createPlaylist(req, res, next) {
   try {
-    const body = req.validated;
-    const playlist = await playlistService.createPlaylistWithSongs(body);
-    res.status(201).json({ status: "success", data: playlist });
+    const { input } = createPlaylistSchema.parse({ input: req.body });
+    const coverFile = req.files?.cover?.[0];
+    const playlist = await playlistService.createPlaylistWithSongs(
+      input,
+      coverFile
+    );
+    res.status(201).json(JSend.success(playlist));
   } catch (err) {
     next(err);
   }
@@ -14,7 +22,7 @@ async function createPlaylist(req, res, next) {
 async function getAllPlaylists(req, res, next) {
   try {
     const playlists = await playlistService.getAllPlaylists();
-    res.status(200).json({ status: "success", data: playlists });
+    res.status(200).json(JSend.success(playlists));
   } catch (err) {
     next(err);
   }
@@ -23,34 +31,29 @@ async function getAllPlaylists(req, res, next) {
 async function deleteAllPlaylists(req, res, next) {
   try {
     await playlistService.deleteAllPlaylists();
-    res.status(200).json({ status: "success", message: "Deleted playlists" });
+    res.status(200).json(JSend.success({ message: "Deleted all playlists" }));
   } catch (err) {
     next(err);
   }
 }
+
+async function updatePlaylist(req, res, next) {
+  try {
+    const { id } = req.params;
+    const { input } = updatePlaylistSchema.parse({ input: req.body });
+    const coverFile = req.files?.cover?.[0];
+    const playlist = await playlistService.updatePlaylist(id, input, coverFile);
+    res.status(200).json(JSend.success(playlist));
+  } catch (err) {
+    next(err);
+  }
+}
+
 async function deletePlaylistById(req, res, next) {
   try {
     const { id } = req.params;
     const result = await playlistService.deletePlaylistById(id);
     res.status(200).json(JSend.success(result));
-  } catch (err) {
-    next(err);
-  }
-}
-async function updatePlaylist(req, res, next) {
-  try {
-    const {
-      params: { id },
-      body,
-    } = req;
-
-    const { input } = updatePlaylistSchema.parse({ input: body });
-
-    const coverFile = req.files?.cover?.[0];
-
-    const playlist = await playlistService.updatePlaylist(id, input, coverFile);
-
-    res.status(200).json({ status: "success", data: playlist });
   } catch (err) {
     next(err);
   }
