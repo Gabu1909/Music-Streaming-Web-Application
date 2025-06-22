8eaf2270f418187e29b5da78cc231b41ef7ea02b
Gabu1909 <kinepone2004@gmail.com>
2025-06-22 15:02:27 +0700

    Update artists.route.js
    commit artist.route.js

--- backend-api/src/routes/artists.route.js
+++ backend-api/src/routes/artists.route.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer"); 
-const upload = multer();          
+const multer = require("multer");
+const upload = multer();
 
 const artistController = require("../controllers/artist.controller");
 
-router.post("/", upload.none(), artistController.createArtist);
+router.post("/createArtist", upload.none(), artistController.createArtist);
+router.delete("/deleteAll", artistController.deleteAllArtists);
+router.get("/getAll", artistController.getAllArtists);
+router.get("/getArtistsByFilter", artistController.getArtistsByFilter);
+router.get("/:id", artistController.getArtistById);
+router.put("/:id", upload.none(), artistController.updateArtistById);
+router.delete("/:id", artistController.deleteArtistById);
 
 module.exports = router;
