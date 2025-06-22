620b3aff0f5e37c7955f2be22e0ec0002243f94d
Gabu1909 <kinepone2004@gmail.com>
2025-06-22 15:45:03 +0700

    Update admin.route.js
    commit admin.route

--- backend-api/src/routes/admin.route.js
+++ backend-api/src/routes/admin.route.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const adminController = require("../controllers/admin.controller");
 
 router.get("/total", adminController.getTotalStats);
-
 router.delete("/delete", adminController.deleteAllUsers);
 
 router.patch("/users/:id/block", adminController.blockUser);
