c0250d3b8af6e2884bcf29d1517cad2bf7c8e2fc
Gabu1909 <kinepone2004@gmail.com>
2025-06-22 15:46:00 +0700

    Update artist.schema.js
    commit artist.schema

--- backend-api/src/schemas/artist.schema.js
+++ backend-api/src/schemas/artist.schema.js
@@ -1,6 +1,5 @@
 const { z } = require("zod");
 
-// Schema cho tạo mới artist
 const createArtistSchema = z.object({
   name: z
     .string({
@@ -12,21 +11,19 @@ const createArtistSchema = z.object({
     .string()
     .regex(/^\d+$/, "User ID must be a number")
     .transform((val) => parseInt(val, 10))
-    .optional(), // Bạn có thể để optional hoặc required tùy yêu cầu
+    .optional(),
 
   bio: z.string().optional(),
 
   avatar_url: z.string().url("Invalid avatar URL").optional(),
 });
 
-// Schema cho cập nhật artist
 const updateArtistSchema = z.object({
   name: z.string().min(1).optional(),
   bio: z.string().optional(),
   avatar_url: z.string().url("Invalid avatar URL").optional(),
 });
 
-// Schema cho artist_id (dùng cho param hoặc path)
 const artistIdSchema = z.object({
   artist_id: z.coerce
     .number()
