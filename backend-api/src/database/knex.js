9c8eb99166ee603b8398cb466d38c9b886b2913a
B2206005 <tuphuongdamdoi@gmail.com>
2025-07-18 16:21:11 +0700

    Update knex.js

--- backend-api/src/database/knex.js
+++ backend-api/src/database/knex.js
@@ -1,5 +1,7 @@
-require("dotenv").config({ path: "../../env" });
+require("dotenv").config(); 
+
 const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
+
 const types = require("pg").types;
 types.setTypeParser(types.builtins.INT8, (value) => {
   return parseInt(value, 10);
