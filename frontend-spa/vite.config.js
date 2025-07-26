c768cc25854210045fab23ab018d09147608cfc5
B2206005 <tuphuongdamdoi@gmail.com>
2025-07-26 11:22:48 +0700

    Update vite.config.js

--- frontend-spa/vite.config.js
+++ frontend-spa/vite.config.js
@@ -4,8 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -15,4 +16,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+
+    },
+  },
 })
+
